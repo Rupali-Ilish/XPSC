ded452a303dcb9fd68ae5ef7a87eba909f15e4e6
Afia Jahin Rupali <163674569+Rupali-Ilish@users.noreply.github.com>
2024-03-30 15:52:57 +0600

    Add files via upload

---
+++ week 2/Day 4/Number Of Segment With Small Sum.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define endl "\n"
+#define pi acos(-1)
+#define pb push_back
+#define pll pair<ll,ll>
+#define NO cout<<"NO"<<endl
+#define YES cout<<"YES"<<endl
+#define fast_io ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+int main()
+{
+    fast_io;
+    ll n,s;
+    cin>>n>>s;
+    vector<ll>v(n);
+    for(ll i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+    ll l=0,r=0;
+    ll sum=0;
+    ll ans=0;
+    ll cnt=0;
+    ll num=0;
+    while(r<n)
+    {
+        sum+=v[r];
+        if(sum<=s)
+        {
+            cnt= r-l+1;
+            //ans=max(ans,cnt);
+            num+= cnt;
+        }
+        else if(sum>s)
+        {
+            while(sum>s and l<r)
+            {
+                sum-=v[l];
+                l++;
+            }
+            if(sum<=s)
+            {
+                cnt= r-l+1;
+                //ans=max(ans,cnt);
+                num+= cnt;
+            }
+        }
+        r++;
+    }
+    cout<<num<<endl;
+}
+
+
